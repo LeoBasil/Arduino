0b6b04559cc939d69470a41568d152bd26ea0a3b
LeoBasil <50512318+LeoBasil@users.noreply.github.com>
2019-05-11 10:44:10 +0200

    Code Cleanup & Temperature displayed

--- clock/clock.ino
+++ clock/clock.ino
@@ -2,59 +2,82 @@
    Project:     Clock
    Hardware:    Arduino Nano - 8 Dig x 7 Seg LED Display - RTC DHT22
    Revision:    1.0
-   Date:        2019-05-10
-   Author:      LeoBasil
+   Date:    2019-05-10
+   Author:  LeoBasil
    License:     CC BY-NC-ND 4.0
-                (Attribution-NonCommercial-NoDerivatives 4.0 International)
+        (Attribution-NonCommercial-NoDerivatives 4.0 International)
 */
 
-#include <LedControl.h>       // Library for LED control with MAX72XX
+#include <LedControl.h>   // Library for LED control with MAX72XX
 #include <Wire.h>  //SDA = A4; SCL = A5
-#include "DS3231.h"
+#include "DS3231.h" // Real Time Clock (RTC) library
 
 // LedControl(int dataPin, int clkPin, int csPin, int numDevices)
 LedControl lc = LedControl(11, 13, 10, 1);
 RTClib RTC;
-
+DateTime now;
+DS3231 Clock;
 
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   Wire.begin();
 
-  lc.shutdown(0, false);      // The MAX72XX is in power-saving mode on startup, we have to do a wakeup call
-  lc.setIntensity(0, 5);      // Set the brightness of display between 0 and 15
+  lc.shutdown(0, false);  // The MAX72XX is in power-saving mode on startup, we have to do a wakeup call
+  lc.setIntensity(0, 5);  // Set the brightness of display between 0 and 15
 
 }
 
 
 void loop() {
-    DateTime now =RTC.now();
-    
+  
+  now = RTC.now();
+  if (now.second()%6<3){
+    display_time();
+  }
+  else{
+    display_temperature();
+  }
+  
+  
 
-    //Serial.println(now.hour());
-    //Serial.println(now.minute());
-    
-      //print month
-      lc.setDigit(0,7,floor(now.month()/10),false);
-      lc.setDigit(0,6,now.month()%10,true);
-
-      //print day
-      lc.setDigit(0,5,floor(now.day()/10),false);
-      lc.setDigit(0,4,now.day()%10,true);
-
-      //print hour;
-      lc.setDigit(0,3,floor(now.hour()/10),false);
-      lc.setDigit(0,2,now.hour()%10,true);
-
-      //print minute;
-      lc.setDigit(0,1,floor(now.minute()/10),false);
-      lc.setDigit(0,0,now.minute()%10,is_even(now.second()));
-
-      delay(1000);
+  delay(1000);
 
 }
 
+void display_temperature(){
+  lc.clearDisplay(0);
+  display_number(Clock.getTemperature(), 0, 2, true);
+  display_number(int((Clock.getTemperature()*10))%10, 0, 1, false);
+  //lc.setChar(0,0,'c', false);
+  lc.setRow(0,0,B1100011);
+}
 
+void display_time(){
+  lc.clearDisplay(0);
+  display_number(now.month(), 0, 4, true);
+  display_number(now.day(), 0, 6, true);
+  display_number(now.hour(), 0, 2, true);
+  display_number(now.minute(), 0 , 0, is_even(now.second()));
+}
+
+void display_number(int number_to_display, int device, int start_position, boolean dp){
+  
+  //Serial.print((int)floor(log10(number_to_display)));
+  int number_of_digits = (int)floor(log10(number_to_display))+1;
+
+  for (int i =0; i<number_of_digits; i++){
+    if (i==0){
+      lc.setDigit(device, start_position + i, get_digit(number_to_display, i) , dp); //for the first digit, use dp parameter for decimal point display
+    }
+    else{
+      lc.setDigit(device, start_position + i, get_digit(number_to_display, i) , false); //for other digits, dont show DP
+    }
+  }
+}
+
+int get_digit(int number, int digit){
+  return (int)floor((int)(floor(number/(pow(10,digit)))) % 10);
+}
 
 boolean is_even(int value){
   return (value%2 == 0);
